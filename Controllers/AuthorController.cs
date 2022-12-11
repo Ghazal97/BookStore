3fc9b859063d86673058d7edcbef9eb809f4ad24
GhazalDaoun <ghazaldaoun@gmail.com>
2022-12-11 12:34:32 +0200

    Add Edit delete services

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -53,16 +53,19 @@ namespace Bookstore.Controllers
         // GET: AuthorController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var author = authorRepository.GetEntity(id);
+           
+            return View(author);
         }
 
         // POST: AuthorController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Author author)
         {
             try
             {
+                authorRepository.Update(id, author);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -74,16 +77,18 @@ namespace Bookstore.Controllers
         // GET: AuthorController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var author = authorRepository.GetEntity(id);
+            return View(author);
         }
 
         // POST: AuthorController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Author author)
         {
             try
             {
+                authorRepository.delete(id);
                 return RedirectToAction(nameof(Index));
             }
             catch
